e14ef6b8088d8b2d37e1d5f674e57b9348512552
rtranKV <rtran@kinetic-vision.com>
2024-12-04 14:10:26 +0000

    learning vectors and pushback

--- src/Ch03/03_10b/CodeDemo.cpp
+++ src/Ch03/03_10b/CodeDemo.cpp
@@ -8,7 +8,15 @@
 #include "cow.h"
 
 int main(){
-    
+    std::vector<cow> cattle;
+
+    cattle.push_back(cow("Betty", 6, cow_purpose::meat));
+    cattle.push_back(cow("Libby", 4, cow_purpose::hide));
+    cattle.push_back(cow("Trudy", 5, cow_purpose::pet));
+    cattle.push_back(cow("Betsy", 2, cow_purpose::dairy));
+
+    std::cout << "The first cow is " << cattle.begin()->get_name() << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
